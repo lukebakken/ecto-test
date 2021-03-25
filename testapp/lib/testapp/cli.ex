7677c8ac925eb324cd1381a4bb21f8e3f320fe84
Luke Bakken <luke@bakken.io>
2021-03-25 15:17:50 -0700

    go boom

--- testapp/lib/testapp/cli.ex
+++ testapp/lib/testapp/cli.ex
@@ -1,6 +1,7 @@
 defmodule TestApp.CLI do
   require Logger
   alias Cart.Item
+  alias TestApp.Repo
 
   def main(args \\ []) do
     Logger.debug("args: #{inspect(args)}")
@@ -8,10 +9,20 @@ defmodule TestApp.CLI do
     started_applications = Application.started_applications()
     Logger.debug("started apps: #{inspect(started_applications)}")
 
-    TestApp.Repo.insert(%Item{name: "Chocolates", price: Decimal.new("5")})
-    TestApp.Repo.insert(%Item{name: "Gum", price: Decimal.new("2.5")})
-    TestApp.Repo.insert(%Item{name: "Milk", price: Decimal.new("1.5")})
-    TestApp.Repo.insert(%Item{name: "Rice", price: Decimal.new("2")})
-    TestApp.Repo.insert(%Item{name: "Chocolates", price: Decimal.new("10")})
+    tx_task =
+      Task.async(fn ->
+        Repo.transaction(fn ->
+          Repo.insert(%Item{name: "Chocolates", price: Decimal.new("5")})
+          Repo.insert(%Item{name: "Gum", price: Decimal.new("2.5")})
+          Repo.insert(%Item{name: "Milk", price: Decimal.new("1.5")})
+          exit(:boom)
+          Repo.insert(%Item{name: "Rice", price: Decimal.new("2")})
+          Repo.insert(%Item{name: "Chocolates", price: Decimal.new("10")})
+          :ok
+        end)
+      end)
+
+    tx_task_result = Task.await(tx_task)
+    Logger.debug("tx_task_result: #{inspect(tx_task_result)}")
   end
 end
