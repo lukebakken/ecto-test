ad0684d8ddaded52612c8d6a3b3fa4c2988d0f81
Luke Bakken <luke@bakken.io>
2021-03-25 13:47:01 -0700

    finally working

--- testapp/lib/testapp/application.ex
+++ testapp/lib/testapp/application.ex
@@ -1,19 +1,12 @@
 defmodule TestApp.Application do
-  # See https://hexdocs.pm/elixir/Application.html
-  # for more information on OTP Applications
   @moduledoc false
-
+  require Logger
   use Application
-
   @impl true
   def start(_type, _args) do
     children = [
-      # Starts a worker by calling: TestApp.Worker.start_link(arg)
-      # {TestApp.Worker, arg}
+      TestApp.Repo
     ]
-
-    # See https://hexdocs.pm/elixir/Supervisor.html
-    # for other strategies and supported options
     opts = [strategy: :one_for_one, name: TestApp.Supervisor]
     Supervisor.start_link(children, opts)
   end
--- testapp/lib/testapp/cli.ex
+++ testapp/lib/testapp/cli.ex
@@ -1,8 +1,13 @@
 defmodule TestApp.CLI do
   require Logger
-  alias Cart.{Item, Invoice, InvoiceItem}
+  alias Cart.Item
+
   def main(args \\ []) do
-    Logger.info("args: #{inspect(args)}")
+    Logger.debug("args: #{inspect(args)}")
+
+    started_applications = Application.started_applications()
+    Logger.debug("started apps: #{inspect(started_applications)}")
+
     TestApp.Repo.insert(%Item{name: "Chocolates", price: Decimal.new("5")})
     TestApp.Repo.insert(%Item{name: "Gum", price: Decimal.new("2.5")})
     TestApp.Repo.insert(%Item{name: "Milk", price: Decimal.new("1.5")})
